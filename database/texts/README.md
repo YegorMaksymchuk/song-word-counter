25f5a6088a211df497fe593b90114afc73eb0025
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-07-12 11:16:48 +0300

    refactor to one project.

---
+++ database/texts/README.md
@@ -0,0 +1,6 @@
+# FORMATTING
+## First row is band or singer name in lowercase and separates by an underscore symbol
+## Second row empty
+## Third-row name of a song
+## First row empty
+## Fifth row is a start of a song text
--- swc-server/src/main/java/com/company/songwordcounter/model/Song.java
+++ swc-server/src/main/java/com/company/songwordcounter/model/Song.java
@@ -2,25 +2,18 @@ package com.company.songwordcounter.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
-@Document(collection = "songs")
 public class Song {
 
-	@Id
+
 	Long id;
-	@Field
 	Actor actor;
-	@Field
 	String title;
-	@Field
 	Set<String> words = new HashSet<String>(0);
 
 	@Override
