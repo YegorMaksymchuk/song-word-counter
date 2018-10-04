12c1658347519f5447311e73b90924422237aa3d
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-10-04 12:47:26 +0300

    add mongo db docker

---
+++ database/docker/mongo-init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+mongo -u mongoadmin -p mongopwd --host localhost --port 27017 --authenticationDatabase admin swcdb
---
+++ database/docker/mongo-run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./mongo-init.sh
+
+./mongo-populate.sh
